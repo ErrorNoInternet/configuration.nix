68981f8dff97eafb49c6400157eaa9817c5aae5c
ErrorNoInternet <errornointernet@envs.net>
2026-01-03 15:59:57 -0500

    nixos/substituters: add nix-community binary cache

--- nixos/modules/substituters.nix
+++ nixos/modules/substituters.nix
@@ -20,16 +20,24 @@ in
     cachix = mkEnableOption "" // {
       default = true;
     };
+
+    nix-community = mkEnableOption "" // {
+      default = true;
+    };
   };
 
   config = mkIf cfg.enable {
     nix.settings = {
-      substituters = optionals cfg.cachix [
-        "https://errornobinaries.cachix.org"
-      ];
-      trusted-public-keys = optionals cfg.cachix [
-        "errornobinaries.cachix.org-1:84oagGNCIsXxBTYmfTiP+lvWje7lIS294iqAtCpFsbU="
-      ];
+      substituters =
+        (optionals cfg.cachix [ "https://errornobinaries.cachix.org" ])
+        ++ (optionals cfg.nix-community [ "https://nix-community.cachix.org" ]);
+      trusted-public-keys =
+        (optionals cfg.cachix [
+          "errornobinaries.cachix.org-1:84oagGNCIsXxBTYmfTiP+lvWje7lIS294iqAtCpFsbU="
+        ])
+        ++ (optionals cfg.nix-community [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ]);
     };
   };
 }
