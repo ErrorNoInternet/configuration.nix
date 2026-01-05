44db2cbabf836b3697707d89eda42b47c6f6f0a0
ErrorNoInternet <errornointernet@envs.net>
2026-01-05 10:51:08 -0500

    agenix: add key for scout

--- agenix/secrets.nix
+++ agenix/secrets.nix
@@ -1,8 +1,10 @@
 let
   moss = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA4uIe2N455VCDagDux58713OyhpCzpDMRJZb0ebxs4g";
+  scout = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZONearVo5X860NvdyGgaaT2Ki8pmY+NRl9/Yj/e7/B";
   zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwJ6RJApteQw7nKq/dAyUC5gMf+q34bFivpBoE5eOeF";
   systems = [
     moss
+    scout
     zenith
   ];
 in
