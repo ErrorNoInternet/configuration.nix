8eeb323fa32e479da806872ef28f48e453a6a5a6
ErrorNoInternet <errornointernet@envs.net>
2026-03-08 19:58:50 -0400

    agenix: update key for scout

--- agenix/secrets.nix
+++ agenix/secrets.nix
@@ -1,6 +1,6 @@
 let
   moss = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA4uIe2N455VCDagDux58713OyhpCzpDMRJZb0ebxs4g";
-  scout = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZONearVo5X860NvdyGgaaT2Ki8pmY+NRl9/Yj/e7/B";
+  scout = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxOfqVk56VD9aifCt5OWqUINP/+q5XjSCTv0SsOErHW";
   zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwJ6RJApteQw7nKq/dAyUC5gMf+q34bFivpBoE5eOeF";
   systems = [
     moss
