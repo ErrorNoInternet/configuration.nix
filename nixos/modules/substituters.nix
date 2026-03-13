a9c3465e759d948f5c3f49568dab9f64a685076f
ErrorNoInternet <errornointernet@envs.net>
2026-03-12 21:08:04 -0400

    nixos/modules/substituters: add garnix

--- nixos/modules/substituters.nix
+++ nixos/modules/substituters.nix
@@ -21,6 +21,10 @@ in
       default = true;
     };
 
+    garnix = mkEnableOption "" // {
+      default = true;
+    };
+
     nix-community = mkEnableOption "" // {
       default = true;
     };
@@ -30,10 +34,14 @@ in
     nix.settings = {
       substituters =
         (optionals cfg.cachix [ "https://errornobinaries.cachix.org" ])
+        ++ (optionals cfg.garnix [ "https://cache.garnix.io" ])
         ++ (optionals cfg.nix-community [ "https://nix-community.cachix.org" ]);
       trusted-public-keys =
         (optionals cfg.cachix [
           "errornobinaries.cachix.org-1:84oagGNCIsXxBTYmfTiP+lvWje7lIS294iqAtCpFsbU="
+        ])
+        ++ (optionals cfg.garnix [
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         ])
         ++ (optionals cfg.nix-community [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
