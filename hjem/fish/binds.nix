1207dcc82160ef7040463c163a1e144367910f06
ErrorNoInternet <errornointernet@envs.net>
2026-03-12 20:13:04 -0400

    hjem/fish/binds: remove function again
    Removing the frozen configuration files in ~/.config/fish seems to
    have resolved these issues.

--- hjem/fish/binds.nix
+++ hjem/fish/binds.nix
@@ -1,12 +1,10 @@
 {
   rum.programs.fish.config = ''
-    function _custom_fish_key_bindings --on-variable fish_key_bindings
-      bind --user ctrl-\\ toggle-comment
-      bind --user ctrl-\] "echo; cdh; commandline -f repaint"
-      bind --user ctrl-alt-e 'commandline -rt -- (commandline -xt | string escape | string join " ")'
-      bind --user ctrl-down history-token-search-forward
-      bind --user ctrl-h backward-kill-word
-      bind --user ctrl-up history-token-search-backward
-    end
+    bind --user ctrl-\\ toggle-comment
+    bind --user ctrl-\] "echo; cdh; commandline -f repaint"
+    bind --user ctrl-alt-e 'commandline -rt -- (commandline -xt | string escape | string join " ")'
+    bind --user ctrl-down history-token-search-forward
+    bind --user ctrl-h backward-kill-word
+    bind --user ctrl-up history-token-search-backward
   '';
 }
