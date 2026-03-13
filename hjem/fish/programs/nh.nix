78da26f00c6256de1711f480a9650cbc076c5008
ErrorNoInternet <errornointernet@envs.net>
2026-03-13 18:44:40 -0400

    hjem/fish/programs/nh: add aliases for regular nh commands

--- hjem/fish/programs/nh.nix
+++ hjem/fish/programs/nh.nix
@@ -2,15 +2,18 @@
   rum.programs.fish.aliases = {
     B = "nh os boot .";
     Ba = "B -a";
+    BB = "nh os boot";
     Bn = "B --no-nom";
     Bna = "B -a --no-nom";
     S = "nh os switch .";
     Sa = "S -a";
     Sn = "S --no-nom";
     Sna = "S -a --no-nom";
+    SS = "nh os switch";
     T = "nh os test .";
     Ta = "T -a";
     Tn = "T --no-nom";
     Tna = "T -a --no-nom";
+    TT = "nh os test";
   };
 }
