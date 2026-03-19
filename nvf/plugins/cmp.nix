e0b2c033293584a4f9f4ba6213eeda12e66765b9
ErrorNoInternet <errornointernet@envs.net>
2026-03-18 20:37:11 -0400

    nvf/plugins/cmp: add default priority for sort

--- nvf/plugins/cmp.nix
+++ nvf/plugins/cmp.nix
@@ -45,16 +45,14 @@ in
           (mkLuaInline /* lua */ ''
             function(a, b)
               local source_priority = {
-                nerdfont = 6,
                 path = 5,
                 lsp = 4,
                 snippets = 3,
                 buffer = 2,
                 yank = 1,
               }
-
-              local a_priority = source_priority[a.source_id]
-              local b_priority = source_priority[b.source_id]
+              local a_priority = source_priority[a.source_id] or 100
+              local b_priority = source_priority[b.source_id] or 100
               if a_priority ~= b_priority then return a_priority > b_priority end
             end
           '')
