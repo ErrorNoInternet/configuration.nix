7d813624247c0d6e06afc38741ab5c86f220f0d9
ErrorNoInternet <errornointernet@envs.net>
2026-03-20 00:17:48 -0400

    hjem/eilmeldung: tweak feed list

--- hjem/eilmeldung.nix
+++ hjem/eilmeldung.nix
@@ -2,9 +2,19 @@
 {
   xdg.config.files = {
     "eilmeldung/config.toml".source = (pkgs.formats.toml { }).generate "config.toml" {
+      article_scope = "all";
+      keep_articles_days = 365;
       mouse_support = true;
       show_top_bar = false;
       sync_every_minutes = 60;
+
+      feed_list = [
+        "query: \"Unread\" unread"
+        "query: \"Marked\" marked"
+        "feeds"
+        "* categories"
+        "tags"
+      ];
 
       feed_list_focused_width = "33%";
       article_list_focused_width = "85%";
