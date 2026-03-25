7c5356c5bd9dc5a18a5872a363122541e283ad99
ErrorNoInternet <errornointernet@envs.net>
2026-03-24 20:21:54 -0400

    hjem/fish/functions: add __fish_list_current_token

--- hjem/fish/functions.nix
+++ hjem/fish/functions.nix
@@ -259,5 +259,23 @@ in
       )' | string trim)
       _tide_print_item jj $tide_jj_icon' ' "$jj_status"
     '';
+
+    __fish_list_current_token = writeFish "__fish_list_current_token.fish" ''
+      function __fish_list_current_token --description 'List contents of token under the cursor if it is a directory, otherwise list the contents of the current directory'
+        set -l val "$(commandline -t | string replace -r '^~' "$HOME")"
+        set -l cmd
+        if test -d $val
+          set cmd l $val
+        else
+          set -l dir (dirname -- $val)
+          if test $dir != . -a -d $dir
+            set cmd l $dir
+          else
+            set cmd l
+          end
+        end
+        __fish_echo $cmd
+      end
+    '';
   };
 }
