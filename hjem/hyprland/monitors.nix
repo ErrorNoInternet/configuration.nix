e4483b6dfe09815be4f93749699318c59c4ef254
ErrorNoInternet <errornointernet@envs.net>
2026-03-30 22:33:00 -0400

    hjem/hyprland/monitors: update for zenith

--- hjem/hyprland/monitors.nix
+++ hjem/hyprland/monitors.nix
@@ -1,7 +1,6 @@
 {
   rum.desktops.hyprland.settings.monitor = [
-    "eDP      , 1920x1080 , 0x0     , 1"
-    "HDMI-A-1 , 1680x1050 , -1680x0 , 1"
-    "         , preferred , auto    , 1"
+    "HDMI-A-1 , 1920x1080@144 , 0x0     , 1"
+    "         , preferred     , auto    , 1"
   ];
 }
