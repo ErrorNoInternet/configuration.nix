6ecefb39e4840ab905cab201ece32c458567a78a
ErrorNoInternet <errornointernet@envs.net>
2026-03-31 15:21:21 -0400

    nixos/hosts/zenith/nvidia: use legacy_580 drivers

--- nixos/hosts/zenith/nvidia.nix
+++ nixos/hosts/zenith/nvidia.nix
@@ -1,7 +1,13 @@
+{ config, ... }:
 {
   hardware = {
     graphics.enable = true;
-    nvidia.open = false;
+
+    nvidia = {
+      open = false;
+      package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+    };
   };
+
   services.xserver.videoDrivers = [ "nvidia" ];
 }
