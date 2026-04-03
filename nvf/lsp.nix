5c5566f9bce74571a0389e601e09165de35ee434
ErrorNoInternet <errornointernet@envs.net>
2026-04-03 18:32:21 -0400

    nvf/lsp: add inlay hints toggle

--- nvf/lsp.nix
+++ nvf/lsp.nix
@@ -9,8 +9,8 @@
 
     keymaps = [
       {
-        action = "<Cmd>lua vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })<CR>";
-        key = "<Leader>T";
+        action = "<Cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<CR>";
+        key = "<Leader>I";
         mode = [
           "n"
           "v"
@@ -19,6 +19,14 @@
       {
         action = "<Cmd>lua vim.diagnostic.config({ virtual_lines = not vim.diagnostic.config().virtual_lines })<CR>";
         key = "<Leader>L";
+        mode = [
+          "n"
+          "v"
+        ];
+      }
+      {
+        action = "<Cmd>lua vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })<CR>";
+        key = "<Leader>T";
         mode = [
           "n"
           "v"
