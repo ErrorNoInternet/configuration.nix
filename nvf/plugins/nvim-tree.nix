c1c0f2cac0c2d759bdae7d8adee3ca849f110989
ErrorNoInternet <errornointernet@envs.net>
2026-04-05 16:09:44 -0400

    nvf: fix terminal and nvim-tree interaction

--- nvf/plugins/nvim-tree.nix
+++ nvf/plugins/nvim-tree.nix
@@ -42,6 +42,39 @@ in
           end
         '';
       }
+      {
+        event = [
+          "BufEnter"
+          "QuitPre"
+        ];
+        callback = mkLuaInline /* lua */ ''
+          function(e)
+            local tree = require('nvim-tree.api').tree
+
+            if not tree.is_visible() then
+              return
+            end
+
+            local winCount = 0
+            for _,winId in ipairs(vim.api.nvim_list_wins()) do
+              if vim.api.nvim_win_get_config(winId).focusable then
+                winCount = winCount + 1
+              end
+            end
+
+            if e.event == 'QuitPre' and winCount == 2 then
+              vim.api.nvim_cmd({cmd = 'qall'}, {})
+            end
+
+            if e.event == 'BufEnter' and winCount == 1 then
+              vim.defer_fn(function()
+                tree.toggle({find_file = true, focus = true})
+                tree.toggle({find_file = true, focus = false})
+              end, 10)
+            end
+          end
+        '';
+      }
     ];
   };
 }
