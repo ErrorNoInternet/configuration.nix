68dca90ed942feaa752e1fc49986714645dddded
ErrorNoInternet <errornointernet@envs.net>
2026-04-14 19:54:52 -0400

    nvf: fix more gitsigns highlights

--- nvf/theme.nix
+++ nvf/theme.nix
@@ -8,12 +8,13 @@ in
 
     luaConfigRC.theme = entryBefore [ "pluginConfigs" "lazyConfigs" ] /* lua */ ''
       vim.cmd.colorscheme("kanagawa")
-    '';
 
-    luaConfigRC.fix-highlights = /* lua */ ''
       vim.api.nvim_set_hl(0, 'GitSignsAdd', { bg = 'NONE', update = true })
       vim.api.nvim_set_hl(0, 'GitSignsChange', { bg = 'NONE', update = true })
       vim.api.nvim_set_hl(0, 'GitSignsDelete', { bg = 'NONE', update = true })
+      vim.api.nvim_set_hl(0, 'GitSignsStagedAdd', { bg = 'NONE', update = true })
+      vim.api.nvim_set_hl(0, 'GitSignsStagedChange', { bg = 'NONE', update = true })
+      vim.api.nvim_set_hl(0, 'GitSignsStagedDelete', { bg = 'NONE', update = true })
     '';
   };
 }
