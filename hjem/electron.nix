42811a383d0f76062a28d3a837c97901db03d6f3
ErrorNoInternet <errornointernet@envs.net>
2026-04-19 15:47:34 -0400

    hjem/electron: update video acceleration flags

--- hjem/electron.nix
+++ hjem/electron.nix
@@ -2,7 +2,7 @@
 {
   xdg.config.files = lib.mkIf osConfig.graphical.enable {
     "electron-flags.conf".text = ''
-      --enable-features=AcceleratedVideoDecodeLinuxGL,AcceleratedVideoDecodeLinuxZeroCopyGL,UseOzonePlatform,VaapiOnNvidiaGPUs,WaylandLinuxDrmSyncobj --ozone-platform-hint=auto --enable-wayland-ime
+      --enable-features=AcceleratedVideoEncoder,VaapiOnNvidiaGPUs,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE --enable-features=VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport --enable-features=UseMultiPlaneFormatForHardwareVideo --ignore-gpu-blocklist --enable-zero-copy
     '';
   };
 }
