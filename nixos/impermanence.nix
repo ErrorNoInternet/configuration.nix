18db314ee97aca8e26361586b81f4deaee765c97
ErrorNoInternet <errornointernet@envs.net>
2026-05-01 21:42:37 -0400

    nixos/impermanence: persist individual /etc/ssh files
    sshd fails to start on first boot otherwise.

--- nixos/impermanence.nix
+++ nixos/impermanence.nix
@@ -63,7 +63,6 @@
     hideMounts = true;
     directories = [
       "/etc/mullvad-vpn"
-      "/etc/ssh"
       "/export"
       "/mnt"
       "/var/db/sudo/lectured"
@@ -77,6 +76,10 @@
     ];
     files = [
       "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
       "/var/lib/cups/printers.conf"
     ];
   };
