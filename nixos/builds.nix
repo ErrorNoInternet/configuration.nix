9e2d0b8d8001bd8c39e3370bde3498828e6dd3ab
ErrorNoInternet <errornointernet@envs.net>
2026-05-02 12:30:22 -0400

    nixos/builds: more parallel building

--- nixos/builds.nix
+++ nixos/builds.nix
@@ -2,9 +2,10 @@
   nix.buildMachines = [
     {
       hostName = "tuxcord.net";
-      maxJobs = 1;
+      maxJobs = 2;
       protocol = "ssh-ng";
       sshUser = "error";
+      supportedFeatures = [ "big-parallel" ];
       system = "x86_64-linux";
     }
   ];
