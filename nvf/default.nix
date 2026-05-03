2d78dd31989317a6d37872c815385d6f479931a4
ErrorNoInternet <errornointernet@envs.net>
2026-05-02 20:28:45 -0400

    nvf: enable osc52 over ssh

--- nvf/default.nix
+++ nvf/default.nix
@@ -16,9 +16,13 @@
     viAlias = false;
     vimAlias = false;
 
-    luaConfigRC.aunmenu = ''
+    luaConfigRC.aunmenu = /* lua */ ''
       vim.cmd.aunmenu("PopUp.-2-")
       vim.cmd.aunmenu("PopUp.How-to\\ disable\\ mouse")
+
+      if vim.env.SSH_TTY ~= nil then
+        vim.g.clipboard = "osc52"
+      end
     '';
   };
 }
