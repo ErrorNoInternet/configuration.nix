e047d47b47234a0d79f24674cf1b417591de884c
ErrorNoInternet <errornointernet@envs.net>
2026-05-03 11:37:42 -0400

    hjem/fuzzel: apply colorscheme

--- hjem/fuzzel.nix
+++ hjem/fuzzel.nix
@@ -3,9 +3,23 @@
   rum.programs.fuzzel = {
     inherit (osConfig.graphical) enable;
 
-    settings.main = {
-      width = 64;
-      font = "${osConfig.fonts.preferredName}:size=${builtins.toString osConfig.fonts.preferredSize}";
+    settings = {
+      main = {
+        width = 64;
+        font = "${osConfig.fonts.preferredName}:size=${
+          builtins.toString (osConfig.fonts.preferredSize + 3)
+        }";
+      };
+
+      colors = with osConfig.scheme; {
+        background = "${base00}ff";
+        text = "${base05}ff";
+        match = "${base0D}ff";
+        selection = "${base03}ff";
+        selection-text = "${base06}ff";
+        selection-match = "${base0D}ff";
+        border = "${base05}ff";
+      };
     };
   };
 }
