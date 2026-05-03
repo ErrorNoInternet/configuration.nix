44b2186e17e2421896d05dd9ceb06a44f41d0703
ErrorNoInternet <errornointernet@envs.net>
2026-05-03 15:42:19 -0400

    nixos/hosts/moss/programs: set kmscon dpmsTimeout to 6 hours

--- nixos/hosts/moss/programs.nix
+++ nixos/hosts/moss/programs.nix
@@ -2,7 +2,10 @@
 {
   development.enable = true;
   display.enable = true;
-  kmscon.enable = true;
+  kmscon = {
+    enable = true;
+    dpmsTimeout = 6 * 60 * 60;
+  };
 
   environment.systemPackages = with pkgs; [
     ffmpeg-full
