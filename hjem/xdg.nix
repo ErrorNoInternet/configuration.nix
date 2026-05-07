63c98d73844401baa9606b8c357f74a730ef06ff
ErrorNoInternet <errornointernet@envs.net>
2026-05-07 18:00:56 -0400

    hjem/xdg: set XDG_PROJECTS_DIR

--- hjem/xdg.nix
+++ hjem/xdg.nix
@@ -5,6 +5,7 @@ let
     XDG_DOWNLOAD_DIR = "$HOME/downloads";
     XDG_MUSIC_DIR = "$HOME";
     XDG_PICTURES_DIR = "$HOME/media/pictures";
+    XDG_PROJECTS_DIR = "$HOME/projects";
     XDG_PUBLICSHARE_DIR = "$HOME";
     XDG_TEMPLATES_DIR = "$HOME";
     XDG_VIDEOS_DIR = "$HOME/media/videos";
