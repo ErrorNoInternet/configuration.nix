3516dcd46839b050627d34ae6a2ec793ebc24184
ErrorNoInternet <errornointernet@envs.net>
2026-05-09 16:37:30 -0400

    nixos/builds: set builders-use-substitutes = true

--- nixos/builds.nix
+++ nixos/builds.nix
@@ -1,12 +1,18 @@
 {
-  nix.buildMachines = [
-    {
-      hostName = "tuxcord.net";
-      maxJobs = 2;
-      protocol = "ssh-ng";
-      sshUser = "error";
-      supportedFeatures = [ "big-parallel" ];
-      system = "x86_64-linux";
-    }
-  ];
+  nix = {
+    extraOptions = /* ini */ ''
+      builders-use-substitutes = true
+    '';
+
+    buildMachines = [
+      {
+        hostName = "tuxcord.net";
+        maxJobs = 2;
+        protocol = "ssh-ng";
+        sshUser = "error";
+        supportedFeatures = [ "big-parallel" ];
+        system = "x86_64-linux";
+      }
+    ];
+  };
 }
