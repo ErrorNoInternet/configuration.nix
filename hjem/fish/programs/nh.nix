88879290385219672b9d8542f4a69a3a8caf9f7c
ErrorNoInternet <errornointernet@envs.net>
2026-05-11 19:13:55 -0400

    hjem/fish/programs/nh: switch to abbrs

--- hjem/fish/programs/nh.nix
+++ hjem/fish/programs/nh.nix
@@ -1,19 +1,24 @@
 {
-  rum.programs.fish.aliases = {
-    B = "nh os boot .";
-    Ba = "B -a";
-    BB = "nh os boot";
-    Bn = "B --no-nom";
-    Bna = "B -a --no-nom";
-    S = "nh os switch .";
-    Sa = "S -a";
-    Sn = "S --no-nom";
-    Sna = "S -a --no-nom";
-    SS = "nh os switch";
-    T = "nh os test .";
-    Ta = "T -a";
-    Tn = "T --no-nom";
-    Tna = "T -a --no-nom";
-    TT = "nh os test";
+  rum.programs.fish = {
+    aliases = {
+      B = "nh os boot .";
+      BB = "nh os boot";
+      S = "nh os switch .";
+      SS = "nh os switch";
+      T = "nh os test .";
+      TT = "nh os test";
+    };
+
+    abbrs = {
+      Ba = "B -a";
+      Bn = "B --no-nom";
+      Bna = "B -a --no-nom";
+      Sa = "S -a";
+      Sn = "S --no-nom";
+      Sna = "S -a --no-nom";
+      Ta = "T -a";
+      Tn = "T --no-nom";
+      Tna = "T -a --no-nom";
+    };
   };
 }
