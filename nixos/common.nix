3547bf6aa8de3eed4b2d857fda99c06071bf86d2
ErrorNoInternet <errornointernet@envs.net>
2026-05-15 23:36:35 -0400

    nixos/services/getty: remove custom lines

--- nixos/common.nix
+++ nixos/common.nix
@@ -129,11 +129,6 @@ in
   upnp.tcpPorts = sshPorts;
 
   services = {
-    getty = {
-      greetingLine = ">>> NixOS ${config.system.nixos.label} (\\m) - \\l";
-      helpLine = mkForce "";
-    };
-
     nscd.enable = false;
     openssh.enable = lib.mkDefault true;
     power-profiles-daemon.enable = true;
