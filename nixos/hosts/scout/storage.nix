13560fd7c675b0a23d2a3529ab5178ce812e2a37
ErrorNoInternet <errornointernet@envs.net>
2026-09-19 16:35:04 -0400

    nixos/hosts/scout/storage: remove moss mount

--- nixos/hosts/scout/storage.nix
+++ nixos/hosts/scout/storage.nix
@@ -1,12 +1,3 @@
 {
   snapper.interval = "0/4:0";
-
-  fileSystems."/mnt/moss" = {
-    device = "moss.local:/export/error";
-    fsType = "nfs";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-    ];
-  };
 }
